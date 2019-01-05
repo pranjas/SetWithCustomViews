19c422c0754fb65cba75f23e686bceb9419a006c
Pranay Kumar Srivastava <pranjas@gmail.com>
2019-01-05 14:14:26 +0530

    Initial Commit

---
+++ SetCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetCardGame
+//
+//  Created by Jasleen Arora Srivastava on 05/01/19.
+//  Copyright © 2019 Jasleen Arora Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
